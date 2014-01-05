cb4444fabce982096c634b9efa24ac649515d553
Revan Sopher <rsopher@gmail.com>
2014-01-05 14:45:57 -0500

    Handle none or multiple faces

--- gabenizer.py
+++ gabenizer.py
@@ -50,55 +50,58 @@ for pic in submissions:
 	photos = response.body['photos']
 	for photo in photos:
 		try:
-			#get image values
-			original_roll = photo['tags'][0]['roll']
-			original_center_x = photo['tags'][0]['center']['x']
-			original_center_y = photo['tags'][0]['center']['y']
-			original_size = photo['tags'][0]['width']
-			original_height = photo['height']
-			original_width = photo['width']
-
-			#hardcoded values for gaben
-			#gaben_roll = -1
-			#gaben_center_x = 47.02
-			#gaben_center_y = 59.28
-			#gaben_size = 51.76
-			#gaben_height = 663
-			#gaben_width = 655
-
-			#hardcoded values for gabenface
-			gaben_roll = -1
-			gaben_center_x = 51.24
-			gaben_center_y = 47.53
-			gaben_size = 67.15
-			gaben_height = 465
-			gaben_width = 484
-
-			#calculate values for scale and position
-			scale = (original_size * original_width) / (gaben_size * gaben_width)
-			scale_height = gaben_height * scale
-			scale_width = gaben_width * scale
-			place_x = (0.01 * original_center_x * original_width) - (0.01 * gaben_center_x * scale_width)
-			place_y = (0.01 * original_center_y * original_height) - (0.01 * gaben_center_y * scale_height)
-
-			print scale
-			print scale_height
-			print scale_width
-			print place_x
-			print place_y
-
-			#open images
 			original = Image.open(cStringIO.StringIO(urllib.urlopen(url).read()))
-			gaben = Image.open('gabenface.png')
-
-			#rotate gaben to match roll
-			gaben = gaben.rotate(int(-1*original_roll))
-
-			#resize gaben
-			gaben = gaben.resize((int(scale_width), int(scale_height)))
-
 			gabenized = original.copy()
-			gabenized.paste(gaben, (int(place_x), int(place_y)), gaben)
+			if not photo['tags']:
+				continue
+			for face in photo['tags']:
+				#get image values
+				original_roll = face['roll']
+				original_center_x = face['center']['x']
+				original_center_y = face['center']['y']
+				original_size = face['width']
+				original_height = photo['height']
+				original_width = photo['width']
+
+				#hardcoded values for gaben
+				#gaben_roll = -1
+				#gaben_center_x = 47.02
+				#gaben_center_y = 59.28
+				#gaben_size = 51.76
+				#gaben_height = 663
+				#gaben_width = 655
+
+				#hardcoded values for gabenface
+				gaben_roll = -1
+				gaben_center_x = 51.24
+				gaben_center_y = 47.53
+				gaben_size = 67.15
+				gaben_height = 465
+				gaben_width = 484
+
+				#calculate values for scale and position
+				scale = (original_size * original_width) / (gaben_size * gaben_width)
+				scale_height = gaben_height * scale
+				scale_width = gaben_width * scale
+				place_x = (0.01 * original_center_x * original_width) - (0.01 * gaben_center_x * scale_width)
+				place_y = (0.01 * original_center_y * original_height) - (0.01 * gaben_center_y * scale_height)
+
+				print scale
+				print scale_height
+				print scale_width
+				print place_x
+				print place_y
+
+				#open image
+				gaben = Image.open('gabenface.png')
+
+				#rotate gaben to match roll
+				gaben = gaben.rotate(int(-1*original_roll))
+
+				#resize gaben
+				gaben = gaben.resize((int(scale_width), int(scale_height)))
+
+				gabenized.paste(gaben, (int(place_x), int(place_y)), gaben)
 
 			final = Image.new("RGB", (original_width * 2, original_height))
 			final.paste(original, (0,0))
