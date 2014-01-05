4a47bd3839d0364c31ca9d04bbcd6606b2c83065
Revan Sopher <rsopher@gmail.com>
2014-01-05 04:39:45 -0500

    Handle variable number of photos per request

--- gabenizer.py
+++ gabenizer.py
@@ -35,6 +35,9 @@ for pic in submissions:
 		continue
 	already_done.append(url)
 	
+	if url == '':
+		continue
+
 	#detect face, get x,y
 	response = unirest.get('http://api.skybiometry.com/fc/faces/detect.json'
 			+'?api_key='+SKYBIO_ID
@@ -44,50 +47,51 @@ for pic in submissions:
 			+'&urls='+url)
 	pprint(vars(response))
 
-	#image manipulation
-	#get image values
-	original_roll = response.body['photos'][0]['tags'][0]['roll']
-	original_center_x = response.body['photos'][0]['tags'][0]['center']['x']
-	original_center_y = response.body['photos'][0]['tags'][0]['center']['y']
-	original_size = response.body['photos'][0]['tags'][0]['width']
-	original_height = response.body['photos'][0]['height']
-	original_width = response.body['photos'][0]['width']
+	photos = response.body['photos']
+	for photo in photos:
+		try:
+			#get image values
+			original_roll = photo['tags'][0]['roll']
+			original_center_x = photo['tags'][0]['center']['x']
+			original_center_y = photo['tags'][0]['center']['y']
+			original_size = photo['tags'][0]['width']
+			original_height = photo['height']
+			original_width = photo['width']
 
-	#hardcoded values for gaben
-	gaben_roll = -1
-	gaben_center_x = 47.02
-	gaben_center_y = 59.28
-	gaben_size = 51.76
-	gaben_height = 663
-	gaben_width = 655
+			#hardcoded values for gaben
+			gaben_roll = -1
+			gaben_center_x = 47.02
+			gaben_center_y = 59.28
+			gaben_size = 51.76
+			gaben_height = 663
+			gaben_width = 655
 
-	#calculate values for scale and position
-	scale = (original_size * original_width) / (gaben_size * gaben_width)
-	scale_height = gaben_height * scale
-	scale_width = gaben_width * scale
-	place_x = (0.01 * original_center_x * original_width) - (0.01 * gaben_center_x * scale_width)
-	place_y = (0.01 * original_center_y * original_height) - (0.01 * gaben_center_y * scale_height)
+			#calculate values for scale and position
+			scale = (original_size * original_width) / (gaben_size * gaben_width)
+			scale_height = gaben_height * scale
+			scale_width = gaben_width * scale
+			place_x = (0.01 * original_center_x * original_width) - (0.01 * gaben_center_x * scale_width)
+			place_y = (0.01 * original_center_y * original_height) - (0.01 * gaben_center_y * scale_height)
 
-	print scale
-	print scale_height
-	print scale_width
-	print place_x
-	print place_y
+			print scale
+			print scale_height
+			print scale_width
+			print place_x
+			print place_y
 
-	#open images
-	try:
-		original = Image.open(cStringIO.StringIO(urllib.urlopen(url).read()))
-		gaben = Image.open('gaben.png')
-	except:
-		continue
+			#open images
+			original = Image.open(cStringIO.StringIO(urllib.urlopen(url).read()))
+			gaben = Image.open('gaben.png')
 
-	#rotate gaben to match roll
-	gaben = gaben.rotate(int(-1*original_roll))
+			#rotate gaben to match roll
+			gaben = gaben.rotate(int(-1*original_roll))
 
-	#resize gaben
-	gaben = gaben.resize((int(scale_width), int(scale_height)))
+			#resize gaben
+			gaben = gaben.resize((int(scale_width), int(scale_height)))
 
-	gabenized = original.copy()
-	gabenized.paste(gaben, (int(place_x), int(place_y)), gaben)
-	gabenized.save(str(time.time())+'gabenized.png')
+			gabenized = original.copy()
+			gabenized.paste(gaben, (int(place_x), int(place_y)), gaben)
+			gabenized.save(str(time.time())+'gabenized.png')
+		except:
+			continue
 
