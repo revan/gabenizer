de80ac1590c1786508e4ef6de367e0b276baa7e1
Revan Sopher <rsopher@gmail.com>
2014-01-06 05:36:57 -0500

    List processed images

--- wsgi/app.py
+++ wsgi/app.py
@@ -1,11 +1,12 @@
-import praw
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    #return render_template('main.html')
-	return 'website under construction'
+def main():
+    return render_template('main.html', images=listdir(os.path.join(
+		os.environ['OPENSHIFT_DATA_DIR']
+		,'pics')))
 
 if __name__ == "__main__":
     app.run()
