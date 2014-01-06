d557b18b4f82b813490f795fa36173892f255b35
Revan Sopher <rsopher@gmail.com>
2014-01-06 06:11:36 -0500

    I forgot return? Really?

--- wsgi/app.py
+++ wsgi/app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template
-app = Flask(__name__, template_folder=os.path.join(os.environ['OPENSHIFT_REPO_DIR'],'wsgi','templates'))
+app = Flask(__name__)
 
 @app.route("/")
 def main():
@@ -8,7 +8,7 @@ def main():
 	try:
 		images=os.listdir(os.path.join(os.environ['OPENSHIFT_DATA_DIR'],'pics'))
 		print images
-		render_template('main.html', images=images)
+		return render_template('main.html', images=images)
 	except:
 		return 'An error has occurred.'
 
