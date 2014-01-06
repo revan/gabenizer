3200718d9210e7403c3fbb12f8d07be377474d51
Revan Sopher <rsopher@gmail.com>
2014-01-05 21:32:55 -0500

    Config

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='gabenizer',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Revan Sopher',
+      author_email='rsopher@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask>=0.7.2', 'MarkupSafe'],
      )
