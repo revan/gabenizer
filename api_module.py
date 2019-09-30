fd9b10b123537128c4ff2baebd14e15ac7d932f6
Revan Sopher <rsopher@gmail.com>
2019-09-30 00:25:45 -0700

    Add overengineered base class for mockable APIs.

---
+++ api_module.py
@@ -0,0 +1,36 @@
+
+import abc
+import inspect
+
+# Setting this to True will cause all henceforth constructed ApiModules to use mock calls.
+use_mocks = False
+
+
+class ApiModule(abc.ABC):
+
+    def __init__(self):
+        """Maybe overridees call() with mocked_call(), checking signatures match."""
+        mocked_call_sig = dict(inspect.signature(self.mocked_call).parameters)
+        original_mocked_call_sig = dict(inspect.signature(ApiModule.mocked_call).parameters)
+        del original_mocked_call_sig['self']
+
+        print(mocked_call_sig, original_mocked_call_sig)
+
+        if original_mocked_call_sig != mocked_call_sig:
+            # We've overridden mocked_call()
+            assert mocked_call_sig == inspect.signature(self.call).parameters
+
+        if use_mocks:
+            self.call = self.mocked_call
+
+    @abc.abstractmethod
+    def call(self, *args, **kwargs):
+        """Call to whatever expensive operations this ApiModule preforms."""
+        pass
+
+    def mocked_call(self, *args, **kwargs):
+        """Optional mock implementation of call(), for use in testing.
+
+        If defined, signature must match call().
+        """
+        pass
